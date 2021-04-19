5cfc2b7413182d8465f9fd9ce6f7589f3f9df16d
macro224 <macro224@163.com>
2021-04-19 15:51:20 +0800

    mac(add): 添加inOf

--- src/type/index.js
+++ src/type/index.js
@@ -1,10 +1,12 @@
 import get from './get.js';
+import isOf from './isOf.js';
 import isArray from './isArray.js';
 import isEmpty from './isEmpty.js';
 import isPrototype from './isPrototype.js';
 
 const modeles = {
     get,
+    isOf,
     isArray,
     isEmpty,
     isPrototype,
